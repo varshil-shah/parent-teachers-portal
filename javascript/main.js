308206fd470b7fc26eeddfd1bee73d181d06582b
Varshil-Shah <varshilashah09@gmail.com>
2021-07-31 19:23:08 +0530

    Initial sidebar

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,11 @@
-// SHOW MENU
-const showMenu = (toggleId, navbarId, bodyId) => {
-  const toggle = document.getElementById(toggleId),
-    navbar = document.getElementById(navbarId),
-    bodypadding = document.getElementById(bodyId);
-
-  if (toggle && navbar) {
-    toggle.addEventListener("click", () => {
-      // APARECER MENU
-      navbar.classList.toggle("show");
-      // ROTATE TOGGLE
-      toggle.classList.toggle("rotate");
-      // PADDING BODY
-      bodypadding.classList.toggle("expander");
-    });
-  }
-};
-showMenu("nav-toggle", "navbar", "body");
-
-// LINK ACTIVE COLOR
+const toggleButton = document.querySelector("#header-toggle");
+const navbar = document.querySelector(".nav");
 const linkColor = document.querySelectorAll(".nav__link");
+
+toggleButton.addEventListener("click", () => {
+  navbar.classList.toggle("show-menu");
+});
+
 function colorLink() {
   linkColor.forEach((l) => l.classList.remove("active"));
   this.classList.add("active");
