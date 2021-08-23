8be723fa0219f91bacab751d394408f05c512370
Varshil-Shah <varshilashah09@gmail.com>
2021-08-23 10:41:24 +0530

    Modify readme.md and sql file

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# Parent-Teacher-Portal
+# Parent-Teachers-Portal
 
-## Introduction
+## ✨ Introduction
+In this project, we have created a website that helps teachers send direct messages to student's parents. For this, we have used HTML and CSS for the front-end, PHP for the back-end, and for the dynamic update for the notices we have used JAVASCRIPT.
+
+## 🔰 Installation:
+1. You must have any server installed on your PC. In my case it was XAMPP.
+2. You have to create a database name "ptp" and upload the sql file provided.
+3. For sending mails, you have to configure 2 files which are located in php and sendmail folder, under xampp folder.
+4. Go to `C:/xampp/php/php.ini` file, open it in any code editor, edit the code as shown below:
+   ![config php.ini file](images/website-img/config-php-ini.PNG)
+5. Go to `C:/xampp/sendmail/sendmail.ini` file, open it in any code editor, edit the code as shown below:
+   ![config sendmail.ini file](images/website-img/config-sendmail-ini-1.PNG)
+   ![config sendmail.ini file](images/website-img/config-sendmail-ini-2.PNG)
+   ![config sendmail.ini file](images/website-img/config-sendmail-ini-3.PNG)
+   ![config sendmail.ini file](images/website-img/config-sendmail-ini-4.PNG)
+6. Now, you have to make that gmail account less secure so that php can use that to send mails.
+7. Last step, some of the php files you have to configure all "YOUR-EMAIL-ADDRESS" text to your particular email-address.
+
+## 💡 Features:
+1. Complete form validation and authentication.
+2. If the user forgets the password, added forgot-password form to reset the password.
+3. Password is stored in encrypted format.
+4. Notices we be refresh automatically after 2mins with reloading the page.
+5. Dynamic search bar.
+6. Easy to use and best user experience.
+
+## About creator
+### Varshil Shah
+Feel free to mail me at my email address [varshilshah1004@gmail.com](mailto:varshilshah1004@gmail.com) for any queries.
--- sql/ptp.sql
+++ sql/ptp.sql
@@ -6,138 +6,118 @@
 -- Generation Time: Aug 20, 2021 at 08:44 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.3.23
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET
+  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+SET
+  time_zone = "+00:00";
+  /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+  /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+  /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+  /*!40101 SET NAMES utf8mb4 */;
 --
--- Database: `ptp`
+  -- Database: `ptp`
+  --
+  -- --------------------------------------------------------
+  --
+  -- Table structure for table `email_send`
+  --
+  CREATE TABLE `email_send` (
+    `id` int(255) NOT NULL,
+    `uniqueId` int(255) NOT NULL,
+    `title` varchar(500) NOT NULL,
+    `message` varchar(2000) NOT NULL,
+    `users` varchar(1500) NOT NULL,
+    `date` varchar(255) NOT NULL
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 --
-
--- --------------------------------------------------------
-
+  -- Dumping data for table `email_send`
+  --
+  -- --------------------------------------------------------
+  --
+  -- Table structure for table `notice`
+  --
+  CREATE TABLE `notice` (
+    `nid` int(255) NOT NULL,
+    `uniqueId` int(255) NOT NULL,
+    `title` varchar(255) NOT NULL,
+    `message` varchar(2000) NOT NULL,
+    `date` varchar(255) NOT NULL,
+    `pdfFile` varchar(255) NOT NULL,
+    `pageName` varchar(255) NOT NULL
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 --
--- Table structure for table `email_send`
+  -- Dumping data for table `notice`
+  --
+  -- --------------------------------------------------------
+  --
+  -- Table structure for table `signup`
+  --
+  CREATE TABLE `signup` (
+    `id` int(255) NOT NULL,
+    `uniqueId` int(255) NOT NULL,
+    `fullName` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `role` varchar(255) NOT NULL,
+    `otp` int(255) NOT NULL,
+    `status` varchar(255) NOT NULL
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 --
-
-CREATE TABLE `email_send` (
-  `id` int(255) NOT NULL,
-  `uniqueId` int(255) NOT NULL,
-  `title` varchar(500) NOT NULL,
-  `message` varchar(2000) NOT NULL,
-  `users` varchar(1500) NOT NULL,
-  `date` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+  -- Dumping data for table `signup`
+  --
+  --
+  -- Indexes for dumped tables
+  --
+  --
+  -- Indexes for table `email_send`
+  --
+ALTER TABLE
+  `email_send`
+ADD
+  PRIMARY KEY (`id`);
 --
--- Dumping data for table `email_send`
+  -- Indexes for table `notice`
+  --
+ALTER TABLE
+  `notice`
+ADD
+  PRIMARY KEY (`nid`);
 --
-
-INSERT INTO `email_send` (`id`, `uniqueId`, `title`, `message`, `users`, `date`) VALUES
-(1, 1147335206, 'Summer Vacation for 40 days ', 'Nibh venenatis cras sed felis eget. Ullamcorper dignissim cras tincidunt lobortis. Sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Tristique sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula. Justo donec enim diam vulputate ut pharetra sit. Suspendisse potenti nullam ac tortor vitae purus faucibus. Elementum nibh tellus molestie nunc', 'fifovah896@5sword.com', '20 Aug, 2021'),
-(18, 1147335206, 'College ends from 1st of november', 'Tincidunt augue interdum velit euismod. Amet nisl suscipit adipiscing bibendum. Sed lectus vestibulum mattis ullamcorper velit sed. Fames ac turpis egestas integer eget aliquet nibh praesent tristique.', 'fifovah896@5sword.com,daxilom157@bushdown.com', '21 Aug, 2021');
-
--- --------------------------------------------------------
-
+  -- Indexes for table `signup`
+  --
+ALTER TABLE
+  `signup`
+ADD
+  PRIMARY KEY (`id`, `uniqueId`);
 --
--- Table structure for table `notice`
+  -- AUTO_INCREMENT for dumped tables
+  --
+  --
+  -- AUTO_INCREMENT for table `email_send`
+  --
+ALTER TABLE
+  `email_send`
+MODIFY
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 19;
 --
-
-CREATE TABLE `notice` (
-  `nid` int(255) NOT NULL,
-  `uniqueId` int(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `message` varchar(2000) NOT NULL,
-  `date` varchar(255) NOT NULL,
-  `pdfFile` varchar(255) NOT NULL,
-  `pageName` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+  -- AUTO_INCREMENT for table `notice`
+  --
+ALTER TABLE
+  `notice`
+MODIFY
+  `nid` int(255) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 2;
 --
--- Dumping data for table `notice`
---
-
-INSERT INTO `notice` (`nid`, `uniqueId`, `title`, `message`, `date`, `pdfFile`, `pageName`) VALUES
-(1, 1147335206, 'College starts from 1st of september', 'Leo duis ut diam quam nulla porttitor massa id neque. At consectetur lorem donec massa sapien. Pellentesque sit amet porttitor eget dolor morbi non arcu risus. Nisl pretium fusce id velit ut. At in tellus integer feugiat. Ac tortor vitae purus faucibus ornare suspendisse sed nisi. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Vivamus at augue eget arcu dictum varius duis at consectetur.', '20 Aug, 2021', '1629481958daily-report-01-08-2021.pdf', 'announcements');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `signup`
---
-
-CREATE TABLE `signup` (
-  `id` int(255) NOT NULL,
-  `uniqueId` int(255) NOT NULL,
-  `fullName` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  `otp` int(255) NOT NULL,
-  `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `signup`
---
-
-INSERT INTO `signup` (`id`, `uniqueId`, `fullName`, `email`, `password`, `role`, `otp`, `status`) VALUES
-(1, 1147335206, 'Varshil Shah', 'varshil.as@somaiya.edu', '$2y$10$.9rvF5k6GSfN3a3PXQ6QS.BynmGJh9ZnY..c1LT8KYXPja1.QZVVe', 'teacher', 361984, 'active'),
-(2, 294310025, 'Random Person', 'fifovah896@5sword.com', '$2y$10$4tfL3puljOF.nr7OShI2tO2t3EyocUDQqXYRGsMXxnzbkWNvn6Eom', 'parent', 842659, 'active'),
-(3, 1055968346, 'Someone Else', 'daxilom157@bushdown.com', '$2y$10$TyMGieskT27tgv.JRN3gG.tN0fc3IoZsfvak4ZrFeNeC9.b4oHJSu', 'parent', 951830, 'active');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `email_send`
---
-ALTER TABLE `email_send`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `notice`
---
-ALTER TABLE `notice`
-  ADD PRIMARY KEY (`nid`);
-
---
--- Indexes for table `signup`
---
-ALTER TABLE `signup`
-  ADD PRIMARY KEY (`id`,`uniqueId`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `email_send`
---
-ALTER TABLE `email_send`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- AUTO_INCREMENT for table `notice`
---
-ALTER TABLE `notice`
-  MODIFY `nid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `signup`
---
-ALTER TABLE `signup`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  -- AUTO_INCREMENT for table `signup`
+  --
+ALTER TABLE
+  `signup`
+MODIFY
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 4;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+  /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+  /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
